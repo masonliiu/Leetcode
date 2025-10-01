5b6ea28c3f936e6b05a5109719cdf282590a2d81
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-01 15:43:59 -0500

    Time: 7 ms (100.00%) | Memory: 44.4 MB (27.96%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n > 0 && Math.pow(3, 19) % n == 0) return true;
-        return false;
+        return n > 0 && 1162261467 % n == 0;
     }
 }
