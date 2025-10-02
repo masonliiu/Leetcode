1a93af3ea6a6d46c08584ad018e3c5efff945cb0
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-02 18:31:12 -0500

    Time: 1 ms (100.00%) | Memory: 42.6 MB (35.08%) - LeetSync

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.java
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 class Solution {
     public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
 
@@ -31,60 +28,5 @@ class Solution {
         }
 
         return result;
-    //     int storage1 = 0;
-    //     int storage2 = 0;
-
-    //     ArrayList<int[]> list1 = new ArrayList<>();
-    //     ArrayList<int[]> list2 = new ArrayList<>();
-    //     ArrayList<int[]> storageList1 = new ArrayList<>();
-    //     ArrayList<int[]> storageList2 = new ArrayList<>();
-
-    //     int bestStart1 = -1;
-
-    //     for (int i = 0; i < nums.length - firstLen + 1; i++) {
-    //         int[] arr = new int[firstLen];
-    //         int sum = 0;
-    //         for (int j = 0; j < firstLen; j++) {
-    //             arr[j] = nums[i + j];
-    //             sum += nums[i + j];
-    //         }
-    //         list1.add(arr);
-    //         if (sum > storage1) {
-    //             storage1 = sum;
-    //             storageList1.clear();
-    //             storageList1.add(arr);
-    //             bestStart1 = i;
-    //         }
-    //     }
-
-    //     for (int[] x : list1) {
-    //         int count = 0;
-    //         for (int g : x) {
-    //             count += g;
-    //         }
-    //         if (count > storage1) {
-    //             storage1 = count;
-    //             storageList1.clear();
-    //             storageList1.add(x);
-    //         }
-    //     }
-
-    //     for (int i = 0; i < nums.length - secondLen + 1; i++) {
-    //         if (!(i + secondLen - 1 < bestStart1 || i > bestStart1 + firstLen - 1)) continue;
-
-    //         int[] arr2 = new int[secondLen];
-    //         int sum = 0;
-    //         for (int j = 0; j < secondLen; j++) {
-    //             arr2[j] = nums[i + j];
-    //             sum += nums[i + j];
-    //         }
-    //         list2.add(arr2);
-    //         if ( sum > storage2) {
-    //             storage2 = sum;
-    //             storageList2.clear();
-    //             storageList2.add(arr2);
-    //         }
-    //     }
-    //     return storage1 + storage2;
     }
 }
