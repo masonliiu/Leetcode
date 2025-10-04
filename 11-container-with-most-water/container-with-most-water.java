5a4deeba152657a5dd0e3b667bff4256c3ced767
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-04 03:15:42 -0500

    Time: 5 ms (77.27%) | Memory: 58.6 MB (8.22%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int storage = Integer.MIN_VALUE;
+        int left = 0;
+        int right = height.length-1;
+
+        while (left < right) {
+            if (Math.abs(right - left) * Math.min(height[right], height[left]) > storage) {
+                storage = Math.abs(right - left) * Math.min(height[right], height[left]);
+            } 
+            if (height[left] < height[right]) {
+                left++;
+                continue;
+            } else {
+                right--;
+                continue;
+            }
+        }
+        return storage;
+
+                // for (int i = 0; i < height.length; i++) {
+        //     for (int x = i; x < height.length; x++) {
+        //         if (Math.abs(x - i) * Math.min(height[x], height[i]) > storage) {
+        //             storage = Math.abs(x - i) * Math.min(height[x], height[i]);
+        //         }
+        //     }
+        // }
+        
+        // if (Math.abs(height.length-1 - 0) * Math.min(height[height.length-1], height[0]) > storage) {
+        //     storage = Math.abs(height.length-1 - 0) * Math.min(height[height.length-1], height[0]);
+        // } 
+        // for (int i = 1; i < height.length; i++) {
+        //     if (Math.abs(height.length-i - i) * Math.min(height[height.length-i], height[i]) > storage) {
+        //             storage = Math.abs(height.length-i - i) * Math.min(height[height.length-i], height[i]);
+        //         }
+        // }
+        // return storage;
+    }
+}
