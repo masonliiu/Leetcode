3eb9d74d2e28aeeee71f16395cd2ef4de76fe2ac
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-04 04:00:29 -0500

    Time: 2 ms (82.97%) | Memory: 60.2 MB (8.73%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,12 +1,14 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
+        long d = 0;
+        for (int i = 0; i < chalk.length; i++) {
+            d += chalk[i];
+        }
+        k %= d;
         int counter = 0;
         while (k >= chalk[counter]) {
             k -= chalk[counter];
             counter++;
-            if (counter == chalk.length) {
-                counter = 0;
-            }
         }
         return counter;
     }
