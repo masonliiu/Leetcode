017592b6aa4de6269cbda6c34fe290a8146cdf38
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-06 03:28:19 -0500

    Time: 1 ms (100.00%) | Memory: 58.2 MB (85.61%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -10,31 +10,51 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int counter = 0;
-        ListNode curr = head;
-        if (head == null || head.next == null) {
-            return head;
+        if (head == null || head.next == null) return head;
+
+        ListNode first = head;
+        ListNode second = head;
+        ListNode storage = head;
+
+        for( int i = 0; i < k; i++) {
+            storage = first;
+            first = first.next;
         }
 
-        while (curr != null) {
-            curr = curr.next;
-            counter++;
+        while (first != null) {
+            first = first.next;
+            second = second.next;
         }
-        curr = head;
+        int temp = storage.val;
+        storage.val = second.val;
+        second.val = temp;
 
-        for ( int i = 0; i < k-1; i++) {
-            curr = curr.next;
-        }
-        ListNode firstK = curr;
-        curr = head;
-
-        for (int i = 1; i < counter - k + 1; i++) {
-            curr = curr.next;
-        }
-        int temp = curr.val;
-        curr.val = firstK.val;
-        firstK.val = temp;
         return head;
+        // int counter = 0;
+        // ListNode curr = head;
+        // if (head == null || head.next == null) {
+        //     return head;
+        // }
+
+        // while (curr != null) {
+        //     curr = curr.next;
+        //     counter++;
+        // }
+        // curr = head;
+
+        // for ( int i = 0; i < k-1; i++) {
+        //     curr = curr.next;
+        // }
+        // ListNode firstK = curr;
+        // curr = head;
+
+        // for (int i = 1; i < counter - k + 1; i++) {
+        //     curr = curr.next;
+        // }
+        // int temp = curr.val;
+        // curr.val = firstK.val;
+        // firstK.val = temp;
+        // return head;
 
 
     }
