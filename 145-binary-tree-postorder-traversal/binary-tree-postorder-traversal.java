82a08742accdff057841ee5abde2a1e22f27be96
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-06 03:54:53 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (29.04%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -6,12 +6,10 @@ class Solution {
         return postOrder(root, arr);
     }
     public List<Integer> postOrder(TreeNode root, List<Integer> arr) {
-        if (root.left != null) {
+        if (root.left != null)
             postOrder(root.left, arr);
-        }
-        if (root.right != null) {
+        if (root.right != null)
             postOrder(root.right, arr);
-        }
         arr.add(root.val);
         return arr;
     }
