8a63bab59ea46bcaba3182523b20360de1476569
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-06 03:45:18 -0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (67.02%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,34 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> arr = new ArrayList<>();
-        if (root == null) {
-            return arr;
-        }
+        
+        if (root == null) return arr;
+
         return inOrder(root, arr);
     }
     public List<Integer> inOrder(TreeNode curr, List<Integer> list) {
-        if (curr.left != null) {
+        if (curr.left != null) 
             inOrder(curr.left, list);
-        }
+
         list.add(curr.val);
-        if (curr.right != null) {
-            inOrder(curr.right, list);   
-        }
+
+        if (curr.right != null) 
+            inOrder(curr.right, list);
 
         return list;
         
