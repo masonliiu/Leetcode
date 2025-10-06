91486155578e3008054b44408eeafcc756dbe609
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-06 03:32:51 -0500

    Time: 1 ms (100.00%) | Memory: 58.3 MB (77.08%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -1,24 +1,17 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
         if (head == null || head.next == null) return head;
 
         ListNode first = head;
         ListNode second = head;
+        ListNode storage = null;
 
-        for( int i = 1; i < k; i++) first = first.next;
-        ListNode storage = first;
+        for( int i = 0; i < k; i++) {
+            storage = first;
+            first = first.next;
+        }
 
-        while (first.next != null) {
+        while (first != null) {
             first = first.next;
             second = second.next;
         }
@@ -27,32 +20,5 @@ class Solution {
         second.val = temp;
 
         return head;
-        // int counter = 0;
-        // ListNode curr = head;
-        // if (head == null || head.next == null) {
-        //     return head;
-        // }
-
-        // while (curr != null) {
-        //     curr = curr.next;
-        //     counter++;
-        // }
-        // curr = head;
-
-        // for ( int i = 0; i < k-1; i++) {
-        //     curr = curr.next;
-        // }
-        // ListNode firstK = curr;
-        // curr = head;
-
-        // for (int i = 1; i < counter - k + 1; i++) {
-        //     curr = curr.next;
-        // }
-        // int temp = curr.val;
-        // curr.val = firstK.val;
-        // firstK.val = temp;
-        // return head;
-
-
     }
 }
