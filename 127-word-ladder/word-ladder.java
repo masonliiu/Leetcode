2e20b5df79f75ef2142e1741cc453ede829d8b56
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-07 02:59:04 -0500

    Time: 80 ms (53.72%) | Memory: 47.9 MB (27.25%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,6 +2,7 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> bankSet = new HashSet<>(wordList);
         if (!bankSet.contains(endWord)) return 0;
+
         Queue<String> q = new LinkedList<>();
         Set<String> visited = new HashSet<>();
         q.offer(beginWord);
@@ -10,13 +11,15 @@ class Solution {
 
         while(!q.isEmpty()) {
             int size = q.size();
+
             for (int i = 0; i < size; i++) {
                 String curr = q.poll();
                 if (curr.equals(endWord)) return level + 1;
-
                 char[] arr = curr.toCharArray();
+
                 for (int pos = 0; pos < arr.length; pos++) {
                     char old = arr[pos];
+                    
                     for (char c = 'a'; c <= 'z'; c++) {
                         if ( c== old) continue;
                         arr[pos] = c;
