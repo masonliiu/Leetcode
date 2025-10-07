1e8496a2e7f02d014ad68ce6e4a0e7c38fd9516f
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-06 20:32:42 -0500

    Time: 3 ms (23.77%) | Memory: 40.9 MB (42.62%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
@@ -8,32 +8,52 @@ class Solution {
             nums.add(Character.getNumericValue(str.charAt(i)));
         }
         
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums.get(i) % 2 == 0) {
-                for (int x = i+1; x < nums.size(); x++) {
-                    if (nums.get(x) % 2 == 0 && nums.get(x) > nums.get(i)) {
-                        storage = nums.get(x);
-                        nums.set(x, nums.get(i));
-                        nums.set(i, storage);
-                        continue;
-                    }
-                }
-            }
-            if (nums.get(i) % 2 != 0) {
-                for (int x = i+1; x < nums.size(); x++) {
-                    if (nums.get(x) % 2 != 0 && nums.get(x) > nums.get(i)) {
-                        storage = nums.get(x);
-                        nums.set(x, nums.get(i));
-                        nums.set(i, storage);
-                        continue;
-                    }
-                }
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+
+        for (int n : nums) {
+            if (n % 2 == 0) {
+                even.add(n);
+            } else {
+                odd.add(n);
             }
         }
-        String s = "";
-        for (int x : nums) {
-            s += x;
+        Collections.sort(even, Collections.reverseOrder());
+        Collections.sort(odd, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        int evens = 0, odds = 0;
+        for (int n : nums) {
+            if (n % 2 == 0) {
+                sb.append(even.get(evens++));
+            } else {
+                sb.append(odd.get(odds++));
+            }
         }
-        return Integer.valueOf(s);
+        return Integer.parseInt(sb.toString());
+
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums.get(i) % 2 == 0) {
+        //         for (int x = i+1; x < nums.size(); x++) {
+        //             if (nums.get(x) % 2 == 0 && nums.get(x) > nums.get(i)) {
+        //                 storage = nums.get(x);
+        //                 nums.set(x, nums.get(i));
+        //                 nums.set(i, storage);
+        //                 continue;
+        //             }
+        //         }
+        //     }
+        //     if (nums.get(i) % 2 != 0) {
+        //         for (int x = i+1; x < nums.size(); x++) {
+        //             if (nums.get(x) % 2 != 0 && nums.get(x) > nums.get(i)) {
+        //                 storage = nums.get(x);
+        //                 nums.set(x, nums.get(i));
+        //                 nums.set(i, storage);
+        //                 continue;
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
