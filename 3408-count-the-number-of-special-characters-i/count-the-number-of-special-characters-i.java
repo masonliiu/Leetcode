25b1025d9a16431ec95cacc510dc1fe82d63810c
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-07 05:53:42 -0500

    Time: 3 ms (47.00%) | Memory: 42.9 MB (26.06%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,25 +1,25 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        Map<Character, Integer> table = new HashMap<>();
         char[] arr = word.toCharArray();
         int count =0;
 
         Set<Character> nums = new HashSet<>();
+        Set<Character> ars = new HashSet<>();
         for (char s : arr) {
             nums.add(s);
         }
         for (char s : nums) {
             if (s == Character.toLowerCase(s)) {
                 if (nums.contains(Character.toUpperCase(s))) {
-                    if (!table.containsKey(Character.toLowerCase(s))) {
-                        table.put(Character.toLowerCase(s), 1);
+                    if (!ars.contains(Character.toLowerCase(s))) {
+                        ars.add(Character.toLowerCase(s));
                         count++;
                     }
                 }
             } else if (s == Character.toUpperCase(s)) {
                 if (nums.contains(Character.toLowerCase(s))) {
-                    if (!table.containsKey(Character.toLowerCase(s))) {
-                        table.put(Character.toLowerCase(s), 1);
+                    if (!ars.contains(Character.toLowerCase(s))) {
+                        ars.add(Character.toLowerCase(s));
                         count++;
                     }
                 }
