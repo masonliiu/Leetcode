a266a325f22a4c342d1279f98e07989cd52c2318
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2025-10-07 05:21:26 -0500

    Time: 3 ms (25.55%) | Memory: 44.9 MB (80.55%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
-        List<Integer> arr = new ArrayList<>(Arrays.stream(friends).boxed().collect(Collectors.toList()));
-        int[] res = new int[friends.length];
+        List<Integer> arr = new ArrayList<>();
+        for (int x : friends) {
+            arr.add(x);
+        }
         int counter=0;
 
 
         for (int x=0; x < order.length; x++) {
             if (arr.contains(order[x])) {
-                res[counter++] = order[x];
+                friends[counter++] = order[x];
             }
         }
-        return res;
+        return friends;
     }
 }
