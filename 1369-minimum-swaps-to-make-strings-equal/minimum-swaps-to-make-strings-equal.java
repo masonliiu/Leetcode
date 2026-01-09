8bb57f4c55f5c153b6e9a5302cc96d503dbd4725
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-08 22:44:03 -0600

    Time: 1 ms (88.15%) | Memory: 42.8 MB (66.35%) - LeetSync

---
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumSwap(String s1, String s2) {
+        int xy = 0;
+        int yx = 0;
+        for (int i = 0; i < s2.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (s1.charAt(i) == 'x') xy++;
+                else yx++;
+            }
+        }
+        if ((xy + yx) % 2 != 0) return -1;
+        int left =0;
+        if ( (xy) % 2 != 0) left++;
+        if ( (yx) % 2 != 0) left++;
+        return xy/2 + yx/2 + left;
+        //yyyxxx
+        //xxyxxy  6
+
+        //xxyyyy  2
+        //xxxyyx
+
+        //if 1 position swap needed, 2 swaps total
+        // if 2 position swap needed, 4 max swaps total
+        // if 3 position swaps needed, 6 max total swaps
+
+
+    }
+}
