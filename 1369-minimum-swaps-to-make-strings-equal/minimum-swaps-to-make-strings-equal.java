bf8c142b04155ec77270c468e7b64eea7dbf55b1
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-08 22:47:49 -0600

    Time: 1 ms (88.15%) | Memory: 43 MB (9.95%) - LeetSync

--- 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.java
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.java
@@ -10,18 +10,6 @@ class Solution {
         }
         if ((xy + yx) % 2 != 0) return -1;
         int left =0;
-        if ( (xy) % 2 != 0) left+=2;
-        return xy/2 + yx/2 + left;
-        //yyyxxx
-        //xxyxxy  6
-
-        //xxyyyy  2
-        //xxxyyx
-
-        //if 1 position swap needed, 2 swaps total
-        // if 2 position swap needed, 4 max swaps total
-        // if 3 position swaps needed, 6 max total swaps
-
-
+        return xy/2 + yx/2 + (xy % 2 != 0 ? 2 : 0);
     }
 }
