28618ea1760cc2131efa97e56b130253c45bc682
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-11 00:49:20 -0600

    Time: 0 ms (100.00%) | Memory: 44.5 MB (15.61%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,25 +1,20 @@
-import java.util.*;
-
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         List<String> stack = new ArrayList<>();
         List<Integer> stack2 = new ArrayList<>();
 
-        int j = 0; 
-
-        for (int i = 1; i <= n && j < target.length; i++) {
+        for (int i = 1, j = 0; i <= n && j < target.length; i++) {
             if (target[j] != i) {
                 stack.add("Push");
                 stack.add("Pop");
                 continue;
             }
-
             stack2.add(i);
             stack.add("Push");
-            j++; 
+            j++;
+
             if (j == target.length) return stack;
         }
-
         return stack;
     }
 }
