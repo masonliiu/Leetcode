55955017c29dd3262c0c73526e1871a73d830d4b
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-13 02:49:14 -0600

    Time: 1 ms (95.11%) | Memory: 45 MB (15.59%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -1,26 +1,9 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
-      int count = 0;
-      int x = 0;
-      int y = 1;
-      while (y < points.length) {
-        if (Math.abs(points[x][0] - points[y][0]) >= 1 && Math.abs(points[x][1] - points[y][1]) >= 1) {
-          count++;
-          points[x][0] = ((points[x][0] - points[y][0]) >= 1) ? --points[x][0] : ++points[x][0];
-          points[x][1] = ((points[x][1] - points[y][1]) >= 1) ? --points[x][1] : ++points[x][1];
-        } else if (Math.abs(points[x][0] - points[y][0]) >= 1) {
-          count++;
-          points[x][0] = ((points[x][0] - points[y][0]) >= 1) ? --points[x][0] : ++points[x][0];
-        } else if (Math.abs(points[x][1] - points[y][1]) >= 1) {
-          count++;
-          points[x][1] = ((points[x][1] - points[y][1]) >= 1) ? --points[x][1] : ++points[x][1];
-        } else {
-          x++;
-          y++;
-        }
+      int answer = 0;
+      for (int i = 1; i < points.length; i++) {
+        answer += Math.max(Math.abs(points[i][0] - points[i-1][0]), Math.abs(points[i][1] - points[i-1][1]));
       }
-
-      return count;
+      return answer;
     }
 }
-// @leet end
