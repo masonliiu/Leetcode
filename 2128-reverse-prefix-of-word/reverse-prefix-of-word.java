793bd4917a0dd3d17e3225c353177794f15fab01
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-15 01:21:39 -0600

    Time: 1 ms (50.12%) | Memory: 42.6 MB (96.66%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        StringBuilder str = new StringBuilder("");
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) ==ch) {
+                str.append(word.charAt(i));
+                str.reverse();
+                return str + word.substring(i+1);
+            }
+            str.append(word.charAt(i));
+        }
+
+        return word;
+    }
+}
