9236e02f7120b30f140b34c3c85b10c0b22f34f7
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-15 01:33:57 -0600

    Time: 0 ms (100.00%) | Memory: 43.1 MB (58.31%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -13,7 +13,6 @@ class Solution {
 
         // return word;
 
-        int i = word.indexOf(ch);
-        return new StringBuilder(word.substring(0, i + 1)).reverse().append(word.substring(i + 1)).toString();
+        return new StringBuilder(word.substring(0, word.indexOf(ch) + 1)).reverse().append(word.substring(word.indexOf(ch) + 1)).toString();
     }
 }
