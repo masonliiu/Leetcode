fa54aa75de49a103672e414bd616563328a9a539
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-16 11:09:52 -0600

    Time: 6 ms (36.34%) | Memory: 44.6 MB (60.51%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -1,16 +1,14 @@
 class Solution {
     public int minimumCost(int[] cost) {
         Arrays.sort(cost);
-        for (int i = 0, j = cost.length-1; i < j; i++, j--) {
-            int storage = cost[i];
-            cost[i] = cost[j];
-            cost[j] = storage;
-        }
-        int sum = 0;
-        if (cost.length == 1) return cost[0];
-        if (cost.length == 2) return cost[0] + cost[1];
-        for (int i = 0; i < cost.length; i++) {
-            if (i % 3 != 2) sum += cost[i];
+        int sum = 0; 
+        int count = 0;
+        for (int i = cost.length-1; i >= 0; i--) {
+            count++;
+            if (count == 3) 
+                count = 0;
+            else 
+                sum += cost[i];
         }
         return sum;
     }
