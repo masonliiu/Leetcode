64398674e996bad399096164163c95d783f1bfdd
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-19 20:28:31 -0600

    Time: 0 ms (100.00%) | Memory: 43.8 MB (14.27%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -2,10 +2,11 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) nums[i] = 51;
-            else count++;
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++; 
+            }
         }
-        Arrays.sort(nums);
         return count; 
     }
 }
