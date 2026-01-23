04e375a05e7778a2977cb339d5cd92d2f26b3b82
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-23 17:19:03 -0600

    Time: 4 ms (94.03%) | Memory: 46.3 MB (72.73%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
@@ -1,23 +1,15 @@
 class Solution {
     public int similarPairs(String[] words) {
         int pairs = 0;
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < words.length; i++) {
-            Set<Character> set1 = new HashSet<>();
-            for (char c : words[i].toCharArray()) {
-                set1.add(c);
+        for (String s : words) {
+            int x = 0;
+            for (char c : s.toCharArray()) {
+                x |= 1 << (c - 'a');
             }
-
-            for (int j = i + 1; j < words.length; j++) {
-                Set<Character> set2 = new HashSet<>();
-                for (char c : words[j].toCharArray()) {
-                    set2.add(c);
-                }
-
-                if (set1.equals(set2)) {
-                    pairs++;
-                }
-            }
+            pairs += map.getOrDefault(x, 0);
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
         return pairs;
