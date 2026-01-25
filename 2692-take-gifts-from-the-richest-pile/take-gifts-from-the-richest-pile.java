5c61620d619ba76dbf1a6f82d5bb36bfec3e48b9
Mason Liu <138553405+masonliiu@users.noreply.github.com>
2026-01-24 19:49:24 -0600

    Time: 9 ms (12.06%) | Memory: 44.6 MB (15.83%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -3,7 +3,7 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         long sum = 0;
         for (int x : gifts) pq.offer(x);
-        for (int j = 0; j < k; j++) pq.offer((int) Math.floor(Math.sqrt(pq.poll())));
+        while (k-->0) pq.offer((int) Math.floor(Math.sqrt(pq.poll())));
         while (!pq.isEmpty()) sum += pq.poll();
         return sum;
     }
